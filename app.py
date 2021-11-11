350d28b1b883571b49160b61bb054b0d7fad0e9c
Picky55 <91838689+Picky55@users.noreply.github.com>
2021-11-11 13:54:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def event_handle(event):
         msg = str(event["message"]["text"])
         if (msg == "สวัสดี") : 
             replyObj = TextSendMessage(text="ดีด้วย")
-        elif(msg == "กินข้าวไหม") : 
+        elif (msg=="กินข้าวไหม") :
             replyObj = TextSendMessage(text="กินดิ")
         else(msg == "ไปเที่ยวไหม") : 
             replyObj = TextSendMessage(text="ไม่จ้า")
